fc18e2fd6b6aca04d5e775a0817949bf9ccc189f
Betsy Haibel <betsy.haibel@gmail.com>
2017-02-17 20:21:17 -0500

    disable links on new passage

--- src/components/TwLink.vue
+++ src/components/TwLink.vue
@@ -1,13 +1,19 @@
 <template>
-  <a href='#' @click.prevent='go'><slot></slot></a>
+  <a :href='active ? "" : null' @click.prevent='go'><slot></slot></a>
 </template>
 
 <script>
 export default {
   name: 'tw-link',
   props: ['to'],
+  computed: {
+    active () {
+      return this.$parent.$parent.active
+    }
+  },
   methods: {
     go () {
+      if (!this.active) return
       if (this.$store.getters.passageForKey(this.to)) {
         this.$store.commit('setCurrentPassage', this.to)
       }
