ca55cb70aa5eab3a6c24e65d77c09beca68e7e54
Betsy Haibel <betsy.haibel@gmail.com>
2017-02-17 20:21:57 -0500

    remove default Vue styles

--- src/App.vue
+++ src/App.vue
@@ -28,13 +28,4 @@ export default {
 tw-storydata {
   display: none;
 }
-
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
